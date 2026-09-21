e48cfef6c7c88eeb0782040ff5cbea79abf44d52
RAZIQ ROSLI <raziqrosli1@gmail.com>
2026-09-22 01:10:17 +0800

    Support both local (XAMPP) and live (InfinityFree) database config

--- config.php
+++ config.php
@@ -1,11 +1,20 @@
 <?php
 // Database connection (mysqli)
 
-// Database settings
-$host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "student_consultation";
+// Detect environment: local (XAMPP) or live (InfinityFree hosting)
+if ($_SERVER['SERVER_NAME'] === 'localhost' || $_SERVER['SERVER_NAME'] === '127.0.0.1') {
+    // Local settings (XAMPP)
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $dbname = "student_consultation";
+} else {
+    // Live settings (InfinityFree hosting)
+    $host = "sql307.infinityfree.com";
+    $user = "if0_42974973";
+    $pass = "raziq0397";
+    $dbname = "if0_42974973_student_consultation";
+}
 
 // Connect to database
 $conn = mysqli_connect($host, $user, $pass, $dbname);
